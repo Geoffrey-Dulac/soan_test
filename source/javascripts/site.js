576dd9bfbb33124c8853277f2db53447d38b5857
Geoffrey Dulac <geoffrey.dulac2@gmail.com>
2021-01-25 20:03:12 +0100

    Finished

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,7 @@
 import displayBills from "./scripts/displayBills.js";
 import handleCheckboxChecked from "./scripts/handleCheckboxChecked.js";
 import handleClickOnPaymentButton from "./scripts/handleClickOnPaymentButton.js";
+import handleLoginSubmit from "./scripts/handleLoginSubmit.js";
 
 
 let bills;
@@ -24,18 +25,32 @@ const checkboxesEventListener = () => {
 } 
 
 const getData = (url) => {
-  fetch(url)
+  try {
+    fetch(url)
     .then((res) => res.json())
     .then(({payments}) => bills = payments)
     .then((res) => displayBills(res))
-    .then((res) => checkboxesEventListener());
+    .then((res) => checkboxesEventListener()); 
+  } catch (error) {
+    console.error(error);
+    document.querySelector('billsToPayArea').innerText = "Service momentanément indisponible ! Si cette gêne persiste, n'hésitez pas à contacter nos équipes.";
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getData('https://demo.soan-solutions.io/test_front/company/CIKLEA/payments'); 
-  const payBtn = document.getElementById('payButton');
-  const btnValidation = document.getElementById("validationPaymentBtn");
 
-  btnValidation.addEventListener('click', handleLastValidation);
-  payBtn.addEventListener('click', handleClickOnPaymentButton);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const indexPage = document.getElementById("indexPage");
+  const loginPage = document.getElementById("loginPage");
+
+  if (indexPage) {
+    getData('https://demo.soan-solutions.io/test_front/company/CIKLEA/payments'); 
+    const payBtn = document.getElementById('payButton');
+    const btnValidation = document.getElementById("validationPaymentBtn");
+    btnValidation.addEventListener('click', handleLastValidation);
+    payBtn.addEventListener('click', handleClickOnPaymentButton); 
+  } else if (loginPage) {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', handleLoginSubmit)
+  }
 })
